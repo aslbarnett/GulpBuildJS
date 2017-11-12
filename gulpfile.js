6b8638678f9493495e52157a106d3e8f28e3c349
Alex <aslbarnett@gmail.com>
2017-11-12 21:52:25 +0000

    added sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const uglify = require('gulp-uglify'); // minify JS
 const concat = require('gulp-concat'); // concat JS
 const sass = require('gulp-sass'); // concat and minify SCSS
 const rename = require('gulp-rename'); // rename files
+const plumber = require('gulp-plumber'); // error handling
+const sourcemaps = require('gulp-sourcemaps'); // sourcmaps
 
 /* -+-+-+----------------------------------+-+-+-
 * FILE PATHS
@@ -25,10 +27,16 @@ gulp.task('styles', () => {
     console.log('starting styles task');
 
     return gulp.src(SASS_PATH)
+        .pipe(plumber(err => {
+            console.log('Styles Task Error: ', err);
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(rename('all.min.css'))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(SASS_DIST_PATH));
 });
 
@@ -37,8 +45,14 @@ gulp.task('scripts', () => {
     console.log('starting scripts task');
 
     return gulp.src(SCRIPTS_PATH)
+        .pipe(plumber(err => {
+            console.log('Scripts Task Error: ', err);
+            this.emit('end');
+        }))
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(concat('all.min.js'))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(SCRIPTS_DIST_PATH));
 });
 
